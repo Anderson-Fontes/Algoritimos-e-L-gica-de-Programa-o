81eee32d97326a3d3a3e925793fcaa5fdf5e65f6
Anderson Fontes Fernandes Júnior <andersonfontes795@gmail.com>
2025-02-11 21:56:24 -0300

    Add files via upload

---
+++ Algoritimo-Aula5-main/aula 5/src/dois copy 2.js
@@ -0,0 +1,2 @@
+numeros = ["um","dois","três","quatro"]
+console.log(numero [3])
---
+++ Algoritimo-Aula5-main/aula 5/src/quatro.js
@@ -0,0 +1,8 @@
+numeros = [8,3,4,7,5]
+soma = 0
+pos = 0
+while(pos < numeros.length){
+    soma = numeros[pos] + soma
+    pos = pos + 1
+}
+console.log(soma)
